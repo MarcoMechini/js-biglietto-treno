218d89ec8c5382298279297312d1f478df906f4b
Marco Mechini <mechinimarco35@gmail.com>
2024-10-28 15:39:56 +0100

    feat: bonus complete

--- js/script.js
+++ js/script.js
@@ -5,28 +5,33 @@ etaUtente = parseInt(etaUtente);
 let kilometriUtente = prompt("Quanti kilometri percorri?");
 kilometriUtente = parseInt(kilometriUtente);
 
-//variabili
 const costoKilometro = 0.21;
 const scMinorenni = 20;
 const scOver65 = 40;
 let costoTotaleBiglietto;
 let scontoEta;
-//LOGICA    
-costoTotaleBiglietto = kilometriUtente * costoKilometro;
-console.log(`Costo base biglietto ${costoTotaleBiglietto}`);
 
+//CONTROLLO DATI INSERITI
+if (isNaN(etaUtente) || isNaN(kilometriUtente)) {
+    console.log(`I dati inseriti non sono corretti per favore inserite un numero`);
+} else {
 
-if (etaUtente < 18) {
-    scontoEta = costoTotaleBiglietto * scMinorenni / 100;
-    costoTotaleBiglietto = costoTotaleBiglietto - scontoEta;
-    console.log(`Hai ricevuto uno sconto del ${scMinorenni}%`);
+    //LOGICA
+    costoTotaleBiglietto = kilometriUtente * costoKilometro;
+    console.log(`Costo base biglietto ${costoTotaleBiglietto}`);
 
-} else if (etaUtente > 65) {
-    scontoEta = costoTotaleBiglietto * scOver65 / 100;
-    costoTotaleBiglietto = costoTotaleBiglietto - scontoEta;
-    console.log(`Hai ricevuto uno sconto del ${scOver65}%`);
-}
+    if (etaUtente < 18) {
+        scontoEta = costoTotaleBiglietto * scMinorenni / 100;
+        costoTotaleBiglietto = costoTotaleBiglietto - scontoEta;
+        console.log(`Hai ricevuto uno sconto del ${scMinorenni}%`);
 
+    } else if (etaUtente > 65) {
+        scontoEta = costoTotaleBiglietto * scOver65 / 100;
+        costoTotaleBiglietto = costoTotaleBiglietto - scontoEta;
+        console.log(`Hai ricevuto uno sconto del ${scOver65}%`);
+    }
 
-costoTotaleBiglietto = costoTotaleBiglietto.toFixed(2);
-console.log(`Il costo del biglietto è ${costoTotaleBiglietto}`);
+    costoTotaleBiglietto = costoTotaleBiglietto.toFixed(2);
+    console.log(`Il costo del biglietto è ${costoTotaleBiglietto}`);
+
+}    
