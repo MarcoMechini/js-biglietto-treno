5eca88be8bc9393880f6cca06462a70b48cab710
Marco Mechini <mechinimarco35@gmail.com>
2024-10-28 16:06:29 +0100

    feat: controllo età negativa refactor: utilizzo di una sola variabile

--- js/script.js
+++ js/script.js
@@ -9,23 +9,26 @@ const costoKilometro = 0.21;
 const scMinorenni = 20;
 const scOver65 = 40;
 let costoTotaleBiglietto;
-let scontoEta;
 
 //CONTROLLO DATI INSERITI
 if (isNaN(etaUtente) || isNaN(kilometriUtente)) {
+
     console.log(`I dati inseriti non sono corretti per favore inserite un numero`);
+
+} else if (etaUtente < 0) {
+
+    console.log('Inserire un età positiva :P');
+
 } else {
 
     //LOGICA
     costoTotaleBiglietto = kilometriUtente * costoKilometro;
 
     if (etaUtente < 18) {
-        scontoEta = costoTotaleBiglietto * scMinorenni / 100;
-        costoTotaleBiglietto = costoTotaleBiglietto - scontoEta;
+        costoTotaleBiglietto = costoTotaleBiglietto - costoTotaleBiglietto * scMinorenni / 100;
 
     } else if (etaUtente > 65) {
-        scontoEta = costoTotaleBiglietto * scOver65 / 100;
-        costoTotaleBiglietto = costoTotaleBiglietto - scontoEta;
+        costoTotaleBiglietto = costoTotaleBiglietto - costoTotaleBiglietto * scOver65 / 100;
     }
 
     costoTotaleBiglietto = costoTotaleBiglietto.toFixed(2);
